0de143ad9e398987484273bbcfb7a4a84adf4e78
TirtaDewi10 <luhmadetirtadewi1234@gmail.com>
2025-11-13 19:41:16 +0800

    Add files via upload

---
+++ topeng_bali_webmap/js/leaflet.js
@@ -0,0 +1,4 @@
+// tiny loader that pulls Leaflet from CDN
+var script = document.createElement('script');
+script.src = 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js';
+document.head.appendChild(script);
